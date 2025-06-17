0cd9b0214cff1a4fa3208ddb908db0c751b04579
saruabh rajput <saurabhraj2509@gmail.com>
2025-06-17 10:20:56 +0530

    formated code

--- lib/Bloc/counter_bloc.dart
+++ lib/Bloc/counter_bloc.dart
@@ -1,16 +1,17 @@
 import 'package:counter/Bloc/counter_event.dart';
 import 'package:counter/Bloc/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-class CounterBloc extends Bloc<CounterEvent, CounterState>{
-  CounterBloc() : super(CounterState(0)){
-    on<IncrementEvent>((event, emit){
-      if(state.count <5) {
-        emit(CounterState(state.count +1 ));
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState(0)) {
+    on<IncrementEvent>((event, emit) {
+      if (state.count < 5) {
+        emit(CounterState(state.count + 1));
       }
     });
     on<DecrementEvent>((event, emit) {
-      if(state.count >0) {
-        emit(CounterState(state.count -1 ));
+      if (state.count > 0) {
+        emit(CounterState(state.count - 1));
       }
     });
   }
